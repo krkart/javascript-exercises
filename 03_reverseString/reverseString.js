ba2ec4db2737a38db501dbfb2cfe7dff55986dca
krkart <krkart@zohomail.in>
2025-02-02 08:48:42 +0530

    ReverseString: all tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+	return [...str].reverse().join("")
 };
+
+// String.prototype.reverse_string=function() {return this.split("").reverse().join("");}
+// console.log(myString.reverse_string())
 
 // Do not edit below this line
 module.exports = reverseString;
