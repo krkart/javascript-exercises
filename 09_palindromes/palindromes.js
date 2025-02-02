60ecc84bd399c218d70ad284f2699f274e461c2c
krkart <krkart@zohomail.in>
2025-02-02 09:08:43 +0530

    Palindromes: all tests passed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+	let strFiltered = str.replace(/[^a-zA-Z0-9]/g, '').toUpperCase();
+	let strReversed = [...strFiltered].reverse().join('');
+	let isPalindrome = strFiltered === strReversed ? true: false;
+	return isPalindrome
 };
 
 // Do not edit below this line
