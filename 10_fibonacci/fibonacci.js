c8ccb2e94e50ce5f169bba88460b490fac836d48
krkart <krkart@zohomail.in>
2025-02-02 10:05:49 +0530

    Fibonacci: all tests passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(int) {
+	int = parseInt(int);
+	if (int < 0) {
+		return "OOPS"
+	} else {
+		let [a, b, c] = [0, 1, 0];
+		for (let i=1; i<int; i++) {
+			c = a + b;
+			a = b;
+			b = c;
+		}
+		return int === 1 ? 1: c
+	}
 };
-
+	
 // Do not edit below this line
 module.exports = fibonacci;
